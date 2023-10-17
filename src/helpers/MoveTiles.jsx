cd0a286dc14243c1d1aa7b556a7ea1ab206bed0d
JamesESS <jesandfordsmith@gmail.com>
2023-10-17 15:58:31 +0100

    feature merge tiles

--- src/helpers/MoveTiles.jsx
+++ src/helpers/MoveTiles.jsx
@@ -1,55 +1,42 @@
-export const moveTiles = (direction, gameBoard, updateGameBoard) => {
-  const newGameBoard = [...gameBoard];
-
-  if (direction === 'left' || direction === 'right') {
-    // Move tiles left or right
-    for (let row = 0; row < newGameBoard.length; row++) {
-      const tiles = newGameBoard[row].filter((tile) => tile !== '');
-      const emptyCells = 4 - tiles.length;
-
-      if (direction === 'right') {
-        for (let i = 0; i < emptyCells; i++) {
-          tiles.unshift('');
-        }
-      } else if (direction === 'left') {
-        for (let i = 0; i < emptyCells; i++) {
-          tiles.push('');
-        }
-      }
-
-      newGameBoard[row] = tiles;
-    }
-  } else if (direction === 'up' || direction === 'down') {
-    // Transpose the board for easier processing
-    const transposedBoard = transpose(newGameBoard);
-
-    // Move tiles up or down
-    for (let col = 0; col < transposedBoard.length; col++) {
-      const tiles = transposedBoard[col].filter((tile) => tile !== '');
-      const emptyCells = 4 - tiles.length;
-
-      if (direction === 'down') {
-        for (let i = 0; i < emptyCells; i++) {
-          tiles.unshift('');
-        }
-      } else if (direction === 'up') {
-        for (let i = 0; i < emptyCells; i++) {
-          tiles.push('');
-        }
-      }
-
-      transposedBoard[col] = tiles;
-    }
-
-    // Transpose the board back to its original orientation
-    return updateGameBoard(transpose(transposedBoard));
-  }
-
-  // Set the state with the updated game board
-  updateGameBoard(newGameBoard);
-};
-
 // Function to transpose a matrix (used for handling 'up' and 'down' directions)
-function transpose(matrix) {
+export function transpose(matrix) {
   return matrix[0].map((_, colIndex) => matrix.map((row) => row[colIndex]));
 }
+
+export function getMergedTiles(tiles) {
+  const mergedTiles = [];
+  for (let i = 0; i < tiles.length; i++) {
+    if (i < tiles.length - 1 && tiles[i] === tiles[i + 1]) {
+      // Merge adjacent tiles when they are equal
+      const newValue = Number(tiles[i]) * 2;
+      mergedTiles.push(newValue);
+      i++; // Skip the next tile since it's already merged
+    } else {
+      // Keep the tile as is
+      mergedTiles.push(tiles[i]);
+    }
+  }
+  return mergedTiles;
+}
+
+export function checkDirection(mergedTiles, direction) {
+  const emptyCells = 4 - mergedTiles.length;
+  if (direction === 'right' || direction === 'down') {
+    for (let i = 0; i < emptyCells; i++) {
+      mergedTiles.unshift('');
+    }
+  } else if (direction === 'left' || direction === 'up') {
+    for (let i = 0; i < emptyCells; i++) {
+      mergedTiles.push('');
+    }
+  }
+}
+
+export const setMergedTiles = (direction, board) => {
+  for (let position = 0; position < board.length; position++) {
+    const tiles = board[position].filter((tile) => tile !== '');
+    const mergedTiles = getMergedTiles(tiles);
+    checkDirection(mergedTiles, direction);
+    board[position] = mergedTiles;
+  }
+};
