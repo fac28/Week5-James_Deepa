5dd54368a61312ae706ecb0432dd0fa2ee18b397
JamesESS <jesandfordsmith@gmail.com>
2023-10-17 11:31:50 +0100

    feature create gameboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 // import { useState } from 'react';
 import './App.css';
-const gameBoard = [["","","",""],["","","",""],["","","",""],["","","",""]]
+const gameBoard = [
+  ['', '', '', ''],
+  ['', '', '', ''],
+  ['', '', '', ''],
+  ['', '', '', ''],
+];
 
 function App() {
   return (
     <>
       <h1>2048</h1>
       <div className='grid-container'>
-        {gameBoard.map((rows,rowIndex) => {
-          return (<div className='grid-row' key={rowIndex}>
-            {rows.map((tileValue,colIndex) => {
-              return (<div className='grid-tile' key={colIndex}>{tileValue}</div>)
-            })}
-          </div>)
+        {gameBoard.map((rows, rowIndex) => {
+          return (
+            <div className='grid-row' key={rowIndex}>
+              {rows.map((tileValue, colIndex) => {
+                return (
+                  <div className='grid-tile' key={colIndex}>
+                    {tileValue}
+                  </div>
+                );
+              })}
+            </div>
+          );
         })}
       </div>
     </>
