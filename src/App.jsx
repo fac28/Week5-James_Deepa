f9d502b4ae43c3c0d57832a634597c23e1f8c5bd
JamesESS <jesandfordsmith@gmail.com>
2023-10-17 11:42:58 +0100

    refactor gameboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
+import { Gameboard } from './assets/Gameboard';
 // import { useState } from 'react';
 import './App.css';
-const gameBoard = [
-  ['', '', '', ''],
-  ['', '', '', ''],
-  ['', '', '', ''],
-  ['', '', '', ''],
-];
+
 
 function App() {
   return (
     <>
       <h1>2048</h1>
-      <div className='grid-container'>
-        {gameBoard.map((rows, rowIndex) => {
-          return (
-            <div className='grid-row' key={rowIndex}>
-              {rows.map((tileValue, colIndex) => {
-                return (
-                  <div className='grid-tile' key={colIndex}>
-                    {tileValue}
-                  </div>
-                );
-              })}
-            </div>
-          );
-        })}
-      </div>
+      <Gameboard />
     </>
   );
 }
---
+++ src/assets/Gameboard.jsx
@@ -0,0 +1,25 @@
+export function Gameboard() {
+    const gameBoard = [
+        ['', '', '', ''],
+        ['', '', '', ''],
+        ['', '', '', ''],
+        ['', '', '', ''],
+      ];
+  return (
+    <div className='grid-container'>
+      {gameBoard.map((rows, rowIndex) => {
+        return (
+          <div className='grid-row' key={rowIndex}>
+            {rows.map((tileValue, colIndex) => {
+              return (
+                <div className='grid-tile' key={colIndex}>
+                  {tileValue}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
